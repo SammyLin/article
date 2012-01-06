5f1f74bda0da59e7203b97f9ddbc9bbd7458335f
Sammy <corn2277@gmail.com>
2012-01-07 00:27:05 +0800

    set categories html

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,24 @@
 class CategoriesController < ApplicationController
+  def show
+    #@category = Category.all( :conditions => [ "name = ?" , params[:slug]]).first
+    #@article = @category.article
+    @articles= Article.all(:joins => :categories, :conditions => { :categories => { :slug => params[:slug]}})
+    if @articles.count == 0
+      redirect_to categories_path
+    else
+
+    respond_to do |format|
+       format.html # show.html.erb
+      format.json { render :json => @article }
+    end
+    end
+
+  end
+  def index
+    @categories = Category.all
+    respond_to do |format|
+      format.html
+      format.json {render :json => @categories}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Tcoc::Application.routes.draw do
     resources :categories
   end
   resources :articles
+  match 'categories/:slug' => 'categories#show'
+  resources :categories
+  
   root :to => "articles#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
